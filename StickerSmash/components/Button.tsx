3db9f5ebbbd9085837af24ecc70ca9c2d5d176d3
melana123 <melana.etec@gmail.com>
2025-03-13 11:37:39 -0300

    Terminando o botao

--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -4,9 +4,10 @@ import FontAwesome from '@expo/vector-icons/FontAwesome'
 type Props = {
     label: string;
     theme?: 'primary';
+    onPress?: () => void;
 }
 
-export default function Button({ label, theme }: Props) { 
+export default function Button({ label, theme, onPress }: Props) { 
   if (theme === 'primary')
   return (
     <View style={[
@@ -14,11 +15,12 @@ export default function Button({ label, theme }: Props) {
       { borderWidth: 4, borderColor:'#ffd33d', borderRadius: 18}
       
       ]}>
-        <Pressable style={[
+        <Pressable style={[ 
           styles.button, 
           {backgroundColor:'#fff'}
+        
 
-        ]} onPress={() => alert("AHHHHHHHHHHHHHHH")}>
+        ]} onPress={(onPress) => alert("AHHHHHHHHHHHHHHH")}>
          <FontAwesome name='picture-o' size={18} color='2529e' style={{ paddingRight: 8 }}/>
           <Text style={[styles.buttonLabel, {color: '#25292e'}]}>{label}</Text>
         </Pressable>
