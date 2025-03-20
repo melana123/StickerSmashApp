bef0ed6cf05ecaaa7cce944d00226269d1695a3c
melana123 <melana.etec@gmail.com>
2025-03-20 11:35:51 -0300

    TA DANDO MUITO ERRO

--- StickerSmash/components/Button.tsx
+++ StickerSmash/components/Button.tsx
@@ -28,7 +28,7 @@ export default function Button({ label, theme, onPress }: Props) {
   )
   return (
     <View style={styles.buttonContainer}>
-        <Pressable style={styles.button} onPress={() => alert("AHHHHHHHHHHHHHHH")}>
+        <Pressable style={styles.button} onPress={onPress}>
           <Text style={styles.buttonLabel}>{label}</Text>
         </Pressable>
     </View>
