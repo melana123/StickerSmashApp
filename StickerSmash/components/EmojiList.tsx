07f05ffb1eaebc25c926f78b54ca3f05853052ba
melana123 <melana.etec@gmail.com>
2025-06-02 08:17:14 -0300

    foi adicionados os emojis

--- StickerSmash/components/EmojiList.tsx
+++ StickerSmash/components/EmojiList.tsx
@@ -15,6 +15,13 @@ export default function EmojiList({onSelect, onCloseModal }: Props) {
     require('../assets/images/emoji4.png'),
     require('../assets/images/emoji5.png'),
     require('../assets/images/emoji6.png'),
+    require('../assets/images/emoji8.png'),
+    require('../assets/images/emoji9.png'),
+    require('../assets/images/emoji10.png'),
+    require('../assets/images/emoji11.png'),
+    require('../assets/images/emoji12.png'),
+    require('../assets/images/emoji13.png'),
+    require('../assets/images/emoji14.png'),
   ])
   return (
     <FlatList
